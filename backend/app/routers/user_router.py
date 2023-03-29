9212b9db5344edaa50075bacc878c34ce19bb1f6
Aki <akilesh.praveen@gmail.com>
2023-03-28 17:28:40 -0700

    feat: add basic user balance interactions

--- backend/app/routers/user_router.py
+++ backend/app/routers/user_router.py
@@ -1,8 +1,33 @@
 from flask import Blueprint, request
 
 from app.services import user_services
+from app.token_required import authorize
 
 user_blueprint = Blueprint('user', __name__)
+
+
+@user_blueprint.route('/add_cash', methods=['POST'])
+@authorize
+def add_cash(username: str):
+    request_json = request.json
+
+    # Request body validation
+    cash = request_json.get('cash', None)
+    if not cash:
+        return 'Provide cash', 400
+    try:
+        cash_int = int(cash)
+    except Exception as e:
+        return 'Invalid cash (integer required)', 400
+
+    # Add cash
+    updated_cash = user_services.add_cash(username, cash_int)
+
+    resp = {
+        'updated_cash': updated_cash
+    }
+
+    return resp, 200
 
 
 @user_blueprint.route('/register', methods=['POST'])
--- backend/app/services/position_services.py
+++ backend/app/services/position_services.py
@@ -1,5 +1,6 @@
 from app.models import position_model, user_model
 from app.models.position_model import db
+from app.services import user_services
 
 VALID_ORDER_TYPES = ['BUY', 'SELL']
 
@@ -15,7 +16,7 @@ def execute_order(
         quantity: int,
 ) -> position_model.Position:
     # get user
-    user = user_model.get_user_by_username(username)
+    user = user_services.get_user_by_username(username)
 
     if order_type == 'BUY':
         return execute_buy_order(
@@ -33,7 +34,7 @@ def execute_sell_order(
         quantity: int
 ) -> position_model.Position:
     # TODO use yfinance to calculate sell price
-    sell_price = 100
+    sell_price = quantity * 100
 
     position = get_position_by_ticker(ticker)
     if not position:
@@ -46,36 +47,42 @@ def execute_sell_order(
         position.quantity = new_quantity
         db.session.add(position)
 
-        # TODO add sell_price to user cash
-        # TODO session.add the updated user cash
+        user.balance += sell_price
+        db.session.add(user)
 
     # commit only if the operations as a whole were successful (to keep them atomic)
     db.session.commit()
     db.session.refresh(position)
-    return position
+    db.session.refresh(user)
+    return {
+        'ticker': position.ticker,
+        'updated_position_quantity': position.quantity,
+        'updated_user_balance': user.balance,
+    }
 
 
 def execute_buy_order(
         user: user_model.User,
         ticker: str,
         quantity: int,
-) -> position_model.Position:
+):
     """Execute the order of order_type with quantity quantity for the provided ticker. Afterwards, return the position after the trade.
     """
     # TODO use yfinance to calculate buy price
-    buy_price = 100
+    buy_price = quantity * 100
+
+    if user.balance < buy_price:
+        raise Exception('Not enough balance to buy')
 
     position = get_position_by_ticker(ticker)
     if not position:
         # Create the position object w/ quantity
         position = position_model.Position(
             ticker=ticker,
-            quantity=quantity
+            quantity=quantity,
+            user_id=user.id
         )
         db.session.add(position)
-
-        # TODO subtract buy_price from user cash
-        # TODO session.add the updated user cash
 
     else:
         current_quantity = position.quantity
@@ -83,13 +90,18 @@ def execute_buy_order(
         position.quantity = new_quantity
         db.session.add(position)
 
-        # TODO subtract buy_price from user cash
-        # TODO session.add the updated user cash
+    user.balance -= buy_price
+    db.session.add(user)
 
     # commit only if the operations as a whole were successful (to keep them atomic)
     db.session.commit()
     db.session.refresh(position)
-    return position
+    db.session.refresh(user)
+    return {
+        'ticker': position.ticker,
+        'updated_position_quantity': position.quantity,
+        'updated_user_balance': user.balance,
+    }
 
 
 def get_position_by_ticker(ticker: str) -> position_model.Position:
