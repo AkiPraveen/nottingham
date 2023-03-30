ff2a10e9adca234061e6ff205d5317a8d7a1daae
Aki <akilesh.praveen@gmail.com>
2023-03-30 15:20:33 -0700

    chore: debug to see if flask actually got installed

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,12 @@
 # install pip dependencies
 pip3 install -r requirements.txt
 
+echo ------------------------------------
+
+echo 'flask version:'
 which flask
+
+echo ------------------------------------
 
 # apply all required migrations to the database
 python3 -m flask db upgrade
